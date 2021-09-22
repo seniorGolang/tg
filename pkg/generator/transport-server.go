250ef8b24619b6e9748484bd5c6250ab44c7ddd5
Alex K <contact@altsoftllc.com>
2021-09-23 07:49:23 +1000

    support global http prefix

--- pkg/generator/transport-server.go
+++ pkg/generator/transport-server.go
@@ -139,7 +139,7 @@ func (tr Transport) serverNewFunc() Code {
 			)
 			bg.Id("srv").Dot("srvHTTP").Op("=").Qual(packageFiber, "New").Call(Id("srv").Dot("config"))
 			if tr.hasJsonRPC {
-				bg.Id("srv").Dot("srvHTTP").Dot("Post").Call(Lit("/"), Id("srv").Dot("serveBatch"))
+				bg.Id("srv").Dot("srvHTTP").Dot("Post").Call(Lit("/"+tr.tags.Value(tagHttpPrefix, "")), Id("srv").Dot("serveBatch"))
 			}
 			bg.For(List(Id("_"), Id("option")).Op(":=").Range().Id("options")).Block(
 				Id("option").Call(Id("srv")),
