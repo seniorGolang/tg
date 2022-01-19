e2799c698c38eec6a00095984cb8aecb90ed8dd1
sattellite <null@sattellite.me>
2022-01-19 12:16:24 +0300

    allow write command options after arguments

--- cmd/tg/main.go
+++ cmd/tg/main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"os/exec"
 	"path"
+	"strings"
 	"time"
 
 	"github.com/urfave/cli/v2"
@@ -57,7 +58,7 @@ func main() {
 				},
 			},
 			ArgsUsage:   "[project name]",
-			UsageText:   "tg init someProject",
+			UsageText:   "tg init [options] projectPath",
 			Description: "init directory structures, basic configuration package",
 		},
 		{
@@ -156,7 +157,11 @@ func main() {
 		},
 	}
 
-	err := app.Run(os.Args)
+	// hack that allows write options after arguments
+	// urfave/cli cannot solve this issue https://github.com/urfave/cli/issues/427
+	// but someone found solution in ipfs/ipget project
+	// https://github.com/urfave/cli/issues/427#issuecomment-712292636
+	err := app.Run(movePostfixOptions(os.Args))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -269,4 +274,33 @@ func cmdSwagger(c *cli.Context) (err error) {
 		}
 	}
 	return
+}
+
+// Function to reorder arguments in "correct" order for urfave/cli
+// Copied from https://github.com/ipfs/ipget/blob/5397b0666d7e90d78c1566ecb90f289dad9d9ec1/main.go#L142
+// And changed start index from 1 to 2.
+func movePostfixOptions(args []string) []string {
+	var endArgs []string
+	for idx := 2; idx < len(args); idx++ {
+		if args[idx][0] == '-' {
+			if !strings.Contains(args[idx], "=") {
+				idx++
+			}
+			continue
+		}
+		if endArgs == nil {
+			// on first write, make copy of args
+			newArgs := make([]string, len(args))
+			copy(newArgs, args)
+			args = newArgs
+		}
+		// add to args accumulator
+		endArgs = append(endArgs, args[idx])
+		// remove from real args list
+		args = args[:idx+copy(args[idx:], args[idx+1:])]
+		idx--
+	}
+
+	// append extracted arguments to the real args
+	return append(args, endArgs...)
 }
