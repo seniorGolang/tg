9c41f16851c65b09f05121ddfac0b72aa0518e55
Alexandr Kozlenkov <sah4ez32@gmail.com>
2023-12-14 22:01:53 +0300

    Fixed type and added parsing bool

--- pkg/generator/method.go
+++ pkg/generator/method.go
@@ -328,6 +328,7 @@ func (m method) argFromString(typeName string, varMap map[string]string, strCode
 			argTypeName := argType.String()
 			if len(argTokens) > 1 {
 				argType = nestedType(vArg.Type, "", argTokens)
+				argTypeName = argType.String()
 			}
 			switch t := argType.(type) {
 			case types.TPointer:
@@ -553,6 +554,8 @@ func (m method) argToTypeConverter(from *Statement, vType types.Type, id *Statem
 	switch *typename {
 	case "string":
 		return id.Op(op).Add(from)
+	case "bool":
+		return List(id, Err()).Op(op).Qual(packageStrconv, "ParseBool").Call(from).Add(errStatement)
 	case "int":
 		return List(id, Err()).Op(op).Qual(packageStrconv, "Atoi").Call(from).Add(errStatement)
 	case "int64":
