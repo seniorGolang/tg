3d36cee0cbe8b2c4781bcf4bcf5a1136048e8bad
AlexK <seniorGolang@gmail.com>
2025-01-16 22:35:26 +0300

    upd client pkg

--- pkg/generator/pkg/httpclient/httpclient.go
+++ pkg/generator/pkg/httpclient/httpclient.go
@@ -3,7 +3,6 @@ package httpclient
 import (
 	"bytes"
 	"context"
-	"crypto/tls"
 	"fmt"
 	"time"
 
@@ -66,48 +65,9 @@ func (c *ClientHTTP) Do(ctx context.Context, req *fasthttp.Request, resp *fastht
 	return
 }
 
-type Option func(*ClientHTTP)
-
-func WithClient(client *fasthttp.Client) Option {
-	return func(c *ClientHTTP) {
-		c.client = client
-	}
-}
-
-func WithTLS(config *tls.Config) Option {
-	return func(c *ClientHTTP) {
-		c.client.TLSConfig = config
-	}
-}
-
-func WithReadTimeout(timeout time.Duration) Option {
-	return func(c *ClientHTTP) {
-		c.client.ReadTimeout = timeout
-	}
-}
-
-func WithWriteTimeout(timeout time.Duration) Option {
-	return func(c *ClientHTTP) {
-		c.client.WriteTimeout = timeout
-	}
-}
-
-func LogRequest() Option {
-	return func(c *ClientHTTP) {
-		c.logRequests = true
-	}
-}
-
-func LogOnError() Option {
-	return func(c *ClientHTTP) {
-		c.logOnError = true
-	}
-}
-
-func HeaderFromCtx(headers ...interface{}) Option {
-	return func(c *ClientHTTP) {
-		c.headersFromCtx = headers
-	}
+func (c *ClientHTTP) SetTimeout(timeout time.Duration) {
+	c.client.ReadTimeout = timeout
+	c.client.WriteTimeout = timeout
 }
 
 func toCurlCommand(req *fasthttp.Request) (*bytes.Buffer, error) {
